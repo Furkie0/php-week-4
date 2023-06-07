240309613562265fa789be7cda6407dcb2546535
Furkie0 <133210182+Furkie0@users.noreply.github.com>
2023-06-07 21:51:03 +0200

    Add files via upload

---
+++ forloop.php
@@ -0,0 +1,51 @@
+<?php 
+
+//opdracht 1
+
+for ($num = 0; $num <= 50; $num++) {
+    echo $num . "\n";
+}
+
+//opdracht 2
+
+
+$klasgenoten = array(
+  "Furkan",
+  "Mohammed",
+  "Cagatay",
+  "Mashall",
+  "Sofyan",
+  "Chovanec",
+  "Joel",
+  "Jules",
+  "Nourdinne",
+  "Amine"
+);
+
+foreach ($klasgenoten as $naam) {
+  echo $naam . "<br>";
+}
+
+
+//opdracht 3
+
+$maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']; 
+
+echo 'Maand 1 is '.$maanden[0].'.<br />';
+echo 'Maand 2 is '.$maanden[1].'.<br />';
+echo 'Maand 3 is '.$maanden[2].'.<br />';
+echo 'Maand 4 is '.$maanden[3].'.<br />';
+echo 'Maand 5 is '.$maanden[4].'.<br />';
+echo 'Maand 6 is '.$maanden[5].'.<br />';
+echo 'Maand 7 is '.$maanden[6].'.<br />';
+echo 'Maand 8 is '.$maanden[7].'.<br />';
+echo 'Maand 9 is '.$maanden[8].'.<br />';
+echo 'Maand 10 is '.$maanden[9].'.<br />';
+echo 'Maand 11 is '.$maanden[10].'.<br />';
+echo 'Maand 12 is '.$maanden[11].'.<br />';
+
+foreach ($maanden as $maand) {
+    echo $maand;
+}
+
+?>
